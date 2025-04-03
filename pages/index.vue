69d724f973bca4db48ae9dc5b451a06db200ead6
chan-max <jackieontheway666@gmail.com>
2025-04-03 20:34:45 +0800

    dev

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const { awesome } = useAppConfig()
+definePageMeta({ layout: 'page' })
+useHead({ titleTemplate: '', title: awesome?.name || 'Nuxt 3 Awesome Starter' })
+
+</script>
+
+<template>
+  <AwesomeWelcome :with-alert="true" />
+</template>
