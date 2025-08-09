55fd8f612435daaec5782219b9b706b4e139a4b0
chanzachary <7514684+chanzachary@user.noreply.gitee.com>
2025-08-09 14:44:43 +0800

    在CategoryTabs组件中新增“抱枕 & 靠枕”和“手机壳”分类及其缩略图，进一步丰富商品分类信息，提升用户体验。

--- components/awesome/CategoryTabs.vue
+++ components/awesome/CategoryTabs.vue
@@ -126,7 +126,10 @@ const getTrendingItems = (categoryIndex: number) => {
           { name: '挂毯', image: '/thumbnail/tapestry.png' },
           { name: '毛巾', image: '/thumbnail/towel.png' },
           { name: '鼠标垫', image: '/thumbnail/mousepad.png' },
-          { name: '帽子', image: '/thumbnail/hat.png' }
+          { name: '帽子', image: '/thumbnail/hat.png' },
+          { name: '抱枕 & 靠枕', image: '/thumbnail/pillow.png' },
+          { name: '手机壳', image: '/thumbnail/phonecover.png' }
+          
         ]
       },
       { 
