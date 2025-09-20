1baa6b2aa78bf854bd4fd5402694a2e1450cc680
chanzachary <7514684+chanzachary@user.noreply.gitee.com>
2025-09-20 16:48:48 +0800

    dev

--- pages/search/components/LoadingSpinner.vue
+++ pages/search/components/LoadingSpinner.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="loading-container">
-    <div class="dot-spinner">
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
+    <div class="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   </div>
 </template>
@@ -18,115 +16,66 @@
 </script>
 
 <style lang="scss" scoped>
-/* From Uiverse.io by abrahamcalsin */
+/* From Uiverse.io by cosnametv */
 .loading-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 300px;
-}
-
-.dot-spinner {
-  --uib-size: 2.8rem;
-  --uib-speed: .9s;
-  --uib-color: #3b82f6;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  height: var(--uib-size);
-  width: var(--uib-size);
-}
-
-.dot-spinner__dot {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  height: 100%;
+  min-height: 60vh;
+  height: 60vh;
   width: 100%;
 }
 
-.dot-spinner__dot::before {
-  content: '';
-  height: 20%;
-  width: 20%;
-  border-radius: 50%;
-  background-color: var(--uib-color);
-  transform: scale(0);
-  opacity: 0.5;
-  animation: pulse0112 calc(var(--uib-speed) * 1.111) ease-in-out infinite;
-  box-shadow: 0 0 20px rgba(59, 130, 246, 0.3);
+.loader {
+  --color: #3b82f6;
+  --size: 50px;
+  width: var(--size);
+  height: var(--size);
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 3px;
 }
 
-.dot-spinner__dot:nth-child(2) {
-  transform: rotate(45deg);
+.loader span {
+  width: 100%;
+  height: 100%;
+  background-color: var(--color);
+  animation: keyframes-blink 0.6s alternate infinite linear;
 }
 
-.dot-spinner__dot:nth-child(2)::before {
-  animation-delay: calc(var(--uib-speed) * -0.875);
+.loader span:nth-child(1) {
+  animation-delay: 0ms;
 }
 
-.dot-spinner__dot:nth-child(3) {
-  transform: rotate(90deg);
+.loader span:nth-child(2) {
+  animation-delay: 200ms;
 }
 
-.dot-spinner__dot:nth-child(3)::before {
-  animation-delay: calc(var(--uib-speed) * -0.75);
+.loader span:nth-child(3) {
+  animation-delay: 300ms;
 }
 
-.dot-spinner__dot:nth-child(4) {
-  transform: rotate(135deg);
+.loader span:nth-child(4) {
+  animation-delay: 400ms;
 }
 
-.dot-spinner__dot:nth-child(4)::before {
-  animation-delay: calc(var(--uib-speed) * -0.625);
+.loader span:nth-child(5) {
+  animation-delay: 500ms;
 }
 
-.dot-spinner__dot:nth-child(5) {
-  transform: rotate(180deg);
+.loader span:nth-child(6) {
+  animation-delay: 600ms;
 }
 
-.dot-spinner__dot:nth-child(5)::before {
-  animation-delay: calc(var(--uib-speed) * -0.5);
-}
-
-.dot-spinner__dot:nth-child(6) {
-  transform: rotate(225deg);
-}
-
-.dot-spinner__dot:nth-child(6)::before {
-  animation-delay: calc(var(--uib-speed) * -0.375);
-}
-
-.dot-spinner__dot:nth-child(7) {
-  transform: rotate(270deg);
-}
-
-.dot-spinner__dot:nth-child(7)::before {
-  animation-delay: calc(var(--uib-speed) * -0.25);
-}
-
-.dot-spinner__dot:nth-child(8) {
-  transform: rotate(315deg);
-}
-
-.dot-spinner__dot:nth-child(8)::before {
-  animation-delay: calc(var(--uib-speed) * -0.125);
-}
-
-@keyframes pulse0112 {
-  0%,
-  100% {
-    transform: scale(0);
-    opacity: 0.5;
+@keyframes keyframes-blink {
+  0% {
+    opacity: 0.3;
+    transform: scale(0.5) rotate(5deg);
   }
 
   50% {
-    transform: scale(1);
     opacity: 1;
+    transform: scale(1);
   }
 }
 </style>
