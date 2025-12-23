9b807159c2cea3807b160713f5c3cf1f7bb8356f
chan-max <jackieontheway666@gmail.com>
2025-12-23 11:49:17 +0800

    ui

--- layouts/page.vue
+++ layouts/page.vue
@@ -433,7 +433,7 @@ onMounted(() => {
 .header-container {
   max-width: 1920px;
   margin: 0 auto;
-  padding: 0;
+  padding: 0 1em;
   display: flex;
   align-items: center;
   justify-content: space-between;
