50741142808dc65279082920558af93e074abcf3
chan-max <jackieontheway666@gmail.com>
2026-07-25 22:19:35 +0800

    chore: ignore sub-app build data in .gitignore v0.0.75

--- apps/shop-a/.data/jackie-nuxt/nuxt/types/nitro-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-export type MiddlewareKey = "product-or-search" | "public-user-auth"
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
-  }
-}
